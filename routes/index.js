e9c302f2946014627097a26f1ad6c9e35858af55
pulkitjadon2003 <pulkitjadon93@gmail.com>
2024-11-02 22:02:21 +0530

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+router.get('/error.ejs', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/:folder/:file', function(req, res, next) {
+  res.render(`${req.params.folder}/${req.params.file}`);
+});
+
+
+
+
+module.exports = router;
