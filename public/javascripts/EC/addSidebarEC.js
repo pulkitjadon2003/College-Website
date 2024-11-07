ab2dda1f568fb249ab1c321162bd36b6f43c415d
pulkitjadon2003 <pulkitjadon93@gmail.com>
2024-11-07 23:47:14 +0530

    Academics com only Dept left

---
+++ public/javascripts/EC/addSidebarEC.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+        
+        changeActivePage()
+        let pagetitle = "Department of Electronics Communication Engineering"
+        document.title = pagetitle
+        document.querySelector('#nameDiv h2').innerHTML = pagetitle
+});
+
+function changeActivePage(params) {
+    let path = document.URL.split('/')
+    let pagename = path[path.length-1]
+    console.log(pagename);
+    let sides = document.querySelectorAll('.sidebar .menu div')
+    sides.forEach(side=>{
+        if (side.getAttribute('onclick').match(pagename)) {
+            side.classList.add('active')  
+        }
+    })
+
+
+function loadPage(page) {
+        window.location.href = page;
+    }
+    
+    
+    
+}
